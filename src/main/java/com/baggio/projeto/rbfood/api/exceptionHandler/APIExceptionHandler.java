a0e3b25a8b3933227d3ed4907b37b6a7b58f4627
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-02-23 16:01:28 -0300

    8.14. Desafio implementando exception handler

--- src/main/java/com/baggio/projeto/rbfood/api/exceptionHandler/APIExceptionHandler.java
+++ src/main/java/com/baggio/projeto/rbfood/api/exceptionHandler/APIExceptionHandler.java
@@ -4,9 +4,11 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.baggio.projeto.rbfood.domain.exception.EntidadeEmUsoException;
 import com.baggio.projeto.rbfood.domain.exception.EntidadeNaoEncontradaException;
 import com.baggio.projeto.rbfood.domain.exception.NegocioException;
 
@@ -31,6 +33,23 @@ public class APIExceptionHandler {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiErro);
 	}
 
+	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+	public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
+		APIErro apiErro = APIErro.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem("O tipo de mídia não é aceito").build();
+		
+		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(apiErro);	
+	}
+	
+	@ExceptionHandler(EntidadeEmUsoException.class)
+	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
+		APIErro apiErro = APIErro.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage()).build();
+		
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(apiErro);	
+	}	
 	
 	
 }
